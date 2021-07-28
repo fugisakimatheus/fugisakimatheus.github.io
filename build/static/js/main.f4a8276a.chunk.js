3d7daa8936e21ee93e26c542031601de59bf516b
Matheus Fugisaki <matheusgfmaciel@gmail.com>
2021-07-28 20:55:50 -0300

    feat add build page

---
+++ build/static/js/main.f4a8276a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppersonal-page"]=this["webpackJsonppersonal-page"]||[]).push([[0],{9:function(e,n,t){"use strict";t.r(n);var r=t(0),c=t.n(r),o=t(3),s=t.n(o),a=t(1);var p=function(){return Object(a.jsx)("h1",{children:"Hello World!"})};s.a.render(Object(a.jsx)(c.a.StrictMode,{children:Object(a.jsx)(p,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.f4a8276a.chunk.js.map
